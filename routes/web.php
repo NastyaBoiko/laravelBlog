bac4e06f6544b2f8273270ad73b2d681b9f31e58
Nastya <killer99whale@mail.ru>
2024-06-05 18:03:03 +0300

    start CRUD category

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController');
 });
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
+
+
 Auth::routes();
 
