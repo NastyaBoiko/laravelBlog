fff297d1c8909e47d02b7eefa4fd9721f8b041a1
Nastya <killer99whale@mail.ru>
2024-06-05 22:24:41 +0300

    admin.category.create

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                        <h1 class="m-0">Добавление категории</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -26,11 +26,14 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-2">
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-outline-primary">Добавить</a>
-                    </div>
                     <div class="col-12">
-                        Категории
+                        <form action="#" class="w-25">
+                            <div class="form-group">
+                                <label for="name">Название</label>
+                                <input type="text" class="form-control" id="name" placeholder="Название категории">
+                            </div>
+                            <input type="submit" class="col-6 btn btn-block btn-outline-primary" value="Добавить">
+                        </form>
                     </div>
                 </div>
                 <!-- /.row -->
